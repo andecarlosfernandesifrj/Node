adcab092ebb1bed2c1463c8bec10c8a68ea3dfa1
andecarlosfernandes <carlosfernandes@ifrj.edu.br>
2022-11-25 10:52:39 -0300

    Versão 4 - Placar do jogo contra Suiça

--- aula1.js
+++ aula1.js
@@ -3,6 +3,7 @@ var dia = require('./biblioteca');
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type':'text/html'});
     res.write('<h2>Brasil 2 x 0 Servia</h2> <br>');
+    res.write('<h3>Brasil 3 x 1 Suiça</h3> <br>');
     res.write('<p style=color:green>'+Date().substring(16,24)+'<br>');
     res.write(dia.diaDaSemana()+'</p><br>')
     res.end('Bom dia!');
